bb9b1311b0d19406725501d00160c99b46f1bc3a
ivanv <overa32@unab.edu.co>
2025-04-24 19:09:41 -0500

    Ecomerce app recargada

---
+++ app/src/main/java/co/edu/unab/ecommerceapptheme/HomeScreen.kt
@@ -0,0 +1,30 @@
+package co.edu.unab.ecommerceapptheme
+
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import co.edu.unab.ecommerceapptheme.ui.theme.EcommerceAppThemeTheme
+
+@Composable
+fun HomeScreen() {
+    Scaffold { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+            Text("home screen", fontSize = 50.sp)
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun homeScreenPreview() {
+    EcommerceAppThemeTheme {
+        HomeScreen()
+        }
+}
---
+++ app/src/main/java/co/edu/unab/ecommerceapptheme/MainActivity.kt
@@ -0,0 +1,47 @@
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import co.edu.unab.ecommerceapptheme.HomeScreen
+import co.edu.unab.ecommerceapptheme.LoginScreen
+import co.edu.unab.ecommerceapptheme.RegisterScreen
+import co.edu.unab.ecommerceapptheme.ui.theme.EcommerceAppThemeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EcommerceAppThemeTheme {
+                val myNavController = rememberNavController()
+                val myStartDestination = "login"
+                NavHost(
+                    navController = myNavController,
+                    startDestination = myStartDestination,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    composable("login") {
+                        LoginScreen(myNavController)
+                    }
+                    composable("register") {
+                        RegisterScreen(myNavController)
+                    }
+                    composable("homescreen") {
+                        HomeScreen()
+                    }
+                }
+            }
+        }
+    }
+}
